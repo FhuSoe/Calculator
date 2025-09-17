8def81ed997c0107486a571a58dac9ae9ab370ec
FhuSoe <rnurmarianto@yahoo.com>
2025-09-17 13:49:01 +0700

    Created the variable to store the numbers and operand, and wrote the function to calculate

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
+//Function for basic math operator
 const add = function(a, b) {
     return a + b;
 };
@@ -12,4 +13,24 @@ const multiply = function(a, b) {
 
 const divide = function(a, b) {
     return a / b;
+};
+
+//Created a variable to update the display later
+let firstNumber = 3;
+let operand = "+";
+let secondNumber = 5;
+
+//The function for calling the which operand to use to calculate.
+const operator = function(firstNumber, operand, secondNumber) {
+    if (operand == "+") {
+        return add(firstNumber, secondNumber);
+    } else if (operand == "-") {
+        return subtract(firstNumber, secondNumber);
+    } else if (operand == "*") {
+        return multiply(firstNumber, secondNumber);
+    } else if (operand == "/" ) {
+        return divide(firstNumber, secondNumber);
+    } else {
+        return console.log("Invalid Operand")
+    }
 };
